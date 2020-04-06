c888e6656257d02d1fc5c0d9e18edf3cf2f80d68
Konstantin F <gasterbiter@mail.ru>
2020-04-06 23:21:08 +0300

    Commit5 Ветка Lesson_2 Удалили 2 файла Пробуем!!!))

--- src/HelloWorldBranch.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorldBranch {
-    public void hello(){
-        System.out.println("Hello World_2");
-    }
-}
--- src/HelloWorldBranch3.java
+++ src/HelloWorldBranch3.java
@@ -1,5 +1,5 @@
 public class HelloWorldBranch3 {
-    private void helloBranch(){
+    private void helloBranch3(){
         String s;
         s = "Hello_Branch_3!!";
         s = null;
